54e205c9b4b7e6d8177494c56e27f59ea92736b4
John Thuneby <thuneby@yahoo.dk>
2024-10-31 19:59:42 +0100

    Added Aspire and two apps

---
+++ src/AspireAlbums.AlbumUI/Services/AlbumService.cs
@@ -0,0 +1,13 @@
+﻿namespace AspireAlbums.AlbumUI.Services
+{
+    public class AlbumService(HttpClient httpClient)
+    {
+        public async Task<IEnumerable<Album>> GetAlbumsAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await httpClient.GetFromJsonAsync<IEnumerable<Album>>("/albums", cancellationToken);
+            return result ?? [];
+        }
+    }
+
+    public record Album(int Id, string Title, string Artist, double Price, string Image_url);
+}
